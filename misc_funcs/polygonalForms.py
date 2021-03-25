b78e4ce5a0a9fc425e0e8865867505a6903a369d
nkyono <38087905+nkyono@users.noreply.github.com>
2021-03-24 16:46:19 -1000

    Update polygonalForms.py

--- misc_funcs/polygonalForms.py
+++ misc_funcs/polygonalForms.py
@@ -1,46 +1,89 @@
 def triNums(minLim, maxLim):
     nums = []
-    for x in range(minLim, maxLim + 1):
-        nums.append(x*(x+1)/2)
+    x = 1
+    while(True):
+        num = int(x*(x+1)/2)
+        if num < maxLim:
+            if num > minLim:
+                nums.append(num)
+        else:
+            break
+        x = x + 1
     return nums
 
 def sqrNums(minLim, maxLim):
     nums = []
-    for x in range(minLim, maxLim + 1):
-        nums.append(x*x)
+    x = 1
+    while(True):
+        num = int(x*x)
+        if num < maxLim:
+            if num > minLim:
+                nums.append(num)
+        else:
+            break
+        x = x + 1
     return nums
     
 def pentNums(minLim, maxLim):
     nums = []
-    for x in range(minLim, maxLim + 1):
-        nums.append(x*(3*x-1)/2)
+    x = 1
+    while(True):
+        num = int(x*(3*x-1)/2)
+        if num < maxLim:
+            if num > minLim:
+                nums.append(num)
+        else:
+            break
+        x = x + 1
     return nums
 
 def hexNums(minLim, maxLim):
     nums = []
-    for x in range(minLim, maxLim + 1):
-        nums.append(x*(2*x-1))
+    x = 1
+    while(True):
+        num = int(x*(2*x-1))
+        if num < maxLim:
+            if num > minLim:
+                nums.append(num)
+        else:
+            break
+        x = x + 1
     return nums
 
 def heptNums(minLim, maxLim):
     nums = []
-    for x in range(minLim, maxLim + 1):
-        nums.append(x*(5*x-3)/2)
+    x = 1
+    while(True):
+        num = int(x*(5*x-3)/2)
+        if num < maxLim:
+            if num > minLim:
+                nums.append(num)
+        else:
+            break
+        x = x + 1
     return nums
 
 def octNums(minLim, maxLim):
     nums = []
-    for x in range(minLim, maxLim + 1):
-        nums.append(x*(3*x-2))
+    x = 1
+    while(True):
+        num = int(x*(3*x-2))
+        if num < maxLim:
+            if num > minLim:
+                nums.append(num)
+        else:
+            break
+        x = x + 1
     return nums
 
 def main():
-    assert triNums(1,5) == [1, 3, 6, 10, 15]
-    assert sqrNums(1,5) == [1, 4, 9, 16, 25]
-    assert pentNums(1,5) == [1, 5, 12, 22, 35]
-    assert hexNums(1,5) == [1, 6, 15, 28, 45]
-    assert heptNums(1,5) == [1, 7, 18, 34, 55]
-    assert octNums(1,5) == [1, 8, 21, 40, 65]
+    assert triNums(0,20) == [1, 3, 6, 10, 15]
+    assert triNums(0,3) == [1]
+    assert sqrNums(0,30) == [1, 4, 9, 16, 25]
+    assert pentNums(0,40) == [1, 5, 12, 22, 35]
+    assert hexNums(0,50) == [1, 6, 15, 28, 45]
+    assert heptNums(0,60) == [1, 7, 18, 34, 55]
+    assert octNums(0,70) == [1, 8, 21, 40, 65]
 
 if __name__ == '__main__':
     main()
