9b99740f52abad4811f6c85d2ee891c38387ee5d
nkyono <38087905+nkyono@users.noreply.github.com>
2021-03-25 15:38:44 -1000

    added a faster way to solve 92

--- lvl_02/prob_092/prob92.py
+++ lvl_02/prob_092/prob92.py
@@ -13,7 +13,7 @@ How many starting numbers below ten million will arrive at 89?
 '''
 
 # took a little long, but idk it gets it done relatively fast ~ 101 seconds
-# should have made a dictionary that marked whether a number goes to 1 or 89, would be much faster
+# should have made a dictionary that marked whether a number goes to 1 or 89, would be much faster ~ 34 seconds (and could be more optomized but want to move on)
 
 def chain(num):
     strNum = str(num)
@@ -25,14 +25,41 @@ def chain(num):
         strNum = str(num)
     return num
 
-
-def main():
+def slow():
     count = 0
+    nums = {}
     for x in range(1,10000001):
         if chain(x) == 89:
             # print(x)
             count = count + 1
     print(count)
+
+def fast():
+    count = 0
+    nums = {}
+    for x in range(1,10000001):
+        num = x
+        strNum = str(x)
+        while num != 89 and num != 1:
+            n = 0
+            for c in strNum:
+                n = n + int(c)**2
+            num = n
+            if num in nums:
+                num = nums[num]
+            strNum = str(num)
+        if num == 89:
+            nums[x] = 89
+            count = count + 1
+        else:
+            nums[x] = 1
+    return num
+
+    print(count)
+
+def main():
+    # slow()
+    fast()
 
 if __name__ == '__main__':
     import time
