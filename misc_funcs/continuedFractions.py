55d3c0097560a06d129d0d43d483ee674727cf24
nkyono <38087905+nkyono@users.noreply.github.com>
2021-04-02 20:50:43 -1000

    Update continuedFractions.py
    add support for irrational numbers

--- misc_funcs/continuedFractions.py
+++ misc_funcs/continuedFractions.py
@@ -14,11 +14,34 @@ def contFrac(a, b):
     arr.append(int(a/b))
     return arr
 
+# a0/1, 
+# (a1a0 + 1)/a1, 
+# (a2(a1a0 + 1) + a0)/(a2a1 + 1), 
+# (a3(a2(a1a0 + 1) + a0) + (a1a0 + 1))/(a3(a2a1 + 1) + a1)
+def contFracIrr(a, b, lim):
+    arr = []
+    rem = a % b
+    while(rem != 0 and len(arr) < lim-1):
+        arr.append(int(a/b))
+        temp = b
+        b = rem
+        a = temp
+        rem = a % b
+    arr.append(int(a/b))
+    return arr
+
 def main():
+    import math
     print(contFrac(415, 93))
     print(contFrac(3, 8))
     print(contFrac(2, 5))
     print(contFrac(1, 3))
+    print(contFracIrr(math.sqrt(2),1,5))
+    print(contFracIrr(math.sqrt(19),1,10))
+    print(contFracIrr(math.pi,1,10))
+    assert contFracIrr(math.sqrt(2),1,5) == [1,2,2,2,2]
+    assert contFracIrr(math.sqrt(19),1,13) == [4,2,1,3,1,2,8,2,1,3,1,2,8]
+    assert contFracIrr(math.pi,1,12) == [3,7,15,1,292,1,1,1,2,1,3,1]
 
 if __name__ == '__main__':
     main()
