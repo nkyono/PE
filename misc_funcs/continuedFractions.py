8e1678f5319d3599d12ab1ed9779b722d0effb37
nkyono <38087905+nkyono@users.noreply.github.com>
2021-04-03 21:12:06 -1000

    Update continuedFractions.py
    added convergent fractions

--- misc_funcs/continuedFractions.py
+++ misc_funcs/continuedFractions.py
@@ -14,10 +14,6 @@ def contFrac(a, b):
     arr.append(int(a/b))
     return arr
 
-# a0/1, 
-# (a1a0 + 1)/a1, 
-# (a2(a1a0 + 1) + a0)/(a2a1 + 1), 
-# (a3(a2(a1a0 + 1) + a0) + (a1a0 + 1))/(a3(a2a1 + 1) + a1)
 def contFracIrr(a, b, lim):
     arr = []
     rem = a % b
@@ -29,6 +25,35 @@ def contFracIrr(a, b, lim):
         rem = a % b
     arr.append(int(a/b))
     return arr
+
+# a0/1, 
+# (a1a0 + 1)/a1, 
+# (a2(a1a0 + 1) + a0)/(a2a1 + 1), 
+# (a3(a2(a1a0 + 1) + a0) + (a1a0 + 1))/(a3(a2a1 + 1) + a1)
+def convergFrac(a, b, lim):
+    arr = []
+    rem = a % b
+    while(rem != 0 and len(arr) < lim-1):
+        arr.append(int(a/b))
+        temp = b
+        b = rem
+        a = temp
+        rem = a % b
+    arr.append(int(a/b))
+    if len(arr) == 0:
+        return
+
+    nums = [arr[0], arr[1]*arr[0] + 1]
+    dems = [1, arr[1]]
+    for x in range(2,lim):
+        nums.append(arr[x]*nums[x-1] + nums[x-2])
+        dems.append(arr[x]*dems[x-1] + dems[x-2])
+    for x in range(len(nums)):
+        print("fraction:",nums[x],"/",dems[x])
+
+
+    return arr
+
 
 def main():
     import math
@@ -42,6 +67,8 @@ def main():
     assert contFracIrr(math.sqrt(2),1,5) == [1,2,2,2,2]
     assert contFracIrr(math.sqrt(19),1,13) == [4,2,1,3,1,2,8,2,1,3,1,2,8]
     assert contFracIrr(math.pi,1,12) == [3,7,15,1,292,1,1,1,2,1,3,1]
+
+    print(convergFrac(math.sqrt(2),1,5))
 
 if __name__ == '__main__':
     main()
