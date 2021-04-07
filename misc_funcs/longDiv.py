ed7657805d27336f6f363bf5b0b38f4582fb3840
nkyono <38087905+nkyono@users.noreply.github.com>
2021-04-06 18:59:07 -1000

    Update longDiv.py
    added long division for square roots

--- misc_funcs/longDiv.py
+++ misc_funcs/longDiv.py
@@ -41,6 +41,55 @@ def longDiv(numerator, denominator, places):
     print(res)
     return res
 
+# sqrt will only take stuff > 1
+def sqrtLongDiv(num, lim):
+    decimalPlace = 0
+    res = ""
+    numStr = str(num)
+    currDividend = ""
+    front = -1
+    back = -1
+    if len(numStr) == 1:
+        front = 0
+        back = 1
+        currDividend = numStr
+    else:
+        front = 0
+        back = 2
+        currDividend = numStr[front: back]
+    divisor = ""
+
+    # finds first digit
+    currDivisor = 1
+    while currDivisor**2 <= int(currDividend):
+        currDivisor = currDivisor + 1
+    currDivisor = currDivisor - 1
+    res = str(currDivisor)
+    remainder = int(currDividend) - currDivisor**2
+    
+    # find rest of the digits
+    while len(res[decimalPlace:]) < lim and remainder != 0:
+        currDividend = str(remainder)
+        currDivisor = str(int(res) * 2)
+        i = 0
+        if back + 2 > len(numStr):
+            if decimalPlace == 0:
+                decimalPlace = len(res)
+            numStr = numStr + "00"
+        front = back
+        back = back + 2
+        currDividend = currDividend + numStr[front:back]
+        while int(currDivisor + str(i)) * i <= int(currDividend):
+            i = i + 1
+        i = i - 1
+        remainder = int(currDividend) - int(currDivisor + str(i)) * i
+        # print(res, remainder, currDividend, currDivisor)
+        res = res + str(i)
+    if decimalPlace != 0:
+        res = res[:decimalPlace] + "." + res[decimalPlace:]
+    print(res)
+    return res
+
 def main():
     import math
     assert longDiv(10, 4, 20) == "2.5"
@@ -48,6 +97,10 @@ def main():
     assert longDiv(1, 3, 20) == "0.33333333333333333333"
     assert longDiv(10, 3, 20) == "3.33333333333333333333"
     assert longDiv(3923, 6173, 257) == "0.63550947675360440628543657864895512716669366596468491819212700469787785517576543009881743074680058318483719423294994330147416167179653329013445650413089259679248339543171877531184189211080511906690426048922728009071764134132512554673578486959339057184513202"
+
+    assert sqrtLongDiv(2,20) == "1.41421356237309504880"
+    assert sqrtLongDiv(1225,10) == "35"
+    assert sqrtLongDiv(4,10) == "2"
 
 if __name__ == '__main__':
     main()
