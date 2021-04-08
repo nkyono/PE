8a38577a6ae6bbe4087123acd531b21621586982
nkyono <38087905+nkyono@users.noreply.github.com>
2021-04-07 18:59:03 -1000

    solved 76

--- lvl_03/prob_076/prob76.py
+++ lvl_03/prob_076/prob76.py
@@ -1,31 +1,6 @@
 '''
 It is possible to write five as a sum in exactly six different ways:
 
-6 + 1
-5 + 1 + 1
-4 + 2 + 1
-3 + 3 + 1
-4 + 1 + 1 + 1
-3 + 2 + 1 + 1
-3 + 1 + 1 + 1 + 1
-2 + 2 + 1 + 1 + 1
-2 + 1 + 1 + 1 + 1 + 1
-1 + 1 + 1 + 1 + 1 + 1 + 1
-5 + 2
-3 + 2 + 2
-4 + 3
-
-
-5 + 1
-4 + 2
-3 + 3
-4 + 1 + 1
-3 + 2 + 1
-3 + 1 + 1 + 1
-2 + 2 + 1 + 1
-2 + 1 + 1 + 1 + 1
-1 + 1 + 1 + 1 + 1 + 1
-
 4 + 1
 3 + 2
 3 + 1 + 1
@@ -33,33 +8,43 @@ It is possible to write five as a sum in exactly six different ways:
 2 + 1 + 1 + 1
 1 + 1 + 1 + 1 + 1
 
-3 + 1
-2 + 2
-2 + 1 + 1
-1 + 1 + 1 + 1
-
-2 + 1
-1 + 1 + 1
-
-1 + 1
-
-0, 1, 2, 4, 6, 9,
-
 How many different ways can one hundred be written as a sum of at least two positive integers?
 '''
+# https://en.wikipedia.org/wiki/Partition
+# https://en.wikipedia.org/wiki/Partition_function_(number_theory)
+# p(n) also includes n itself so we need p(n)-1 I think
+import math
+
+def pentNums(minLim, maxLim):
+    nums = []
+    x = 1
+    while(True):
+        num = int(x*(3*x-1)/2)
+        if num < maxLim:
+            if num > minLim:
+                nums.append(num)
+        else:
+            break
+        x = x + 1
+    return nums
 
 def calcWays(lim):
-    ways = [0 for x in range(lim)]
-    ways[2] = 1
-    for x in range(3,lim):
-        sum = ways[x-1] + 1
-        for y in range(1,int(x/2)+1):
-            sum = sum + int(y/2)
-        ways[x] = sum
-    print(ways)
-    
+    partN = [1,1]
+    for x in range(2,lim):
+        sum = 0
+        kLow = int((-1) * ((math.sqrt(24*x + 1) - 1) / 6))
+        kHigh = int((math.sqrt(24*x + 1) + 1) / 6)
+        for k in range(kLow, kHigh+1):
+            if k != 0:
+                check = int(x - k*(3*k - 1) / 2)
+                if check >= 0:
+                    sum = sum + ((-1)**(k+1) * partN[int(x - k * (3 * k - 1) / 2)])
+        partN.append(int(sum))
+    for x in range(len(partN)):
+        print(x, partN[x]-1)
+
 def test():
-    calcWays(100)
+    calcWays(101)
 
 if __name__ == '__main__':
     import time
