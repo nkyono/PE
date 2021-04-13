9819a4d2a52797a6e061a117a2895d1dd38ca98e
nkyono <38087905+nkyono@users.noreply.github.com>
2021-04-12 21:59:02 -1000

    created divisor function

--- misc_funcs/calcDivisors.py
+++ misc_funcs/calcDivisors.py
@@ -11,7 +11,7 @@ def calcDivisorsBool(target):
                 return False
     return True
 
-# returns list of divisors
+# returns list of divisors (not including 1 and self)
 def calcDivisorsList(target):
     if target < 0:
         return []
@@ -24,3 +24,34 @@ def calcDivisorsList(target):
                 divisors.append(i)
                 divisors.append(target/i)
     return divisors
+
+# returns divs including 1 and self
+def calcDivisorsListSelf(target):
+    if target < 0:
+        return []
+    divisors = [1, target]
+    for i in range(2,int(target**(1/2))+1):
+        if target % i == 0:
+            if target/i == i:
+                divisors.append(i)
+            else:
+                divisors.append(i)
+                divisors.append(target/i)
+    return divisors
+
+def divisorFunc(n, k):
+    divs = calcDivisorsListSelf(n)
+    total = 0
+    for x in divs:
+        total = total + x**k
+    return int(total)
+
+def main():
+    assert divisorFunc(140,1) == 336
+    assert divisorFunc(140,2) == 27300
+    assert divisorFunc(140,3) == 3164112
+    assert divisorFunc(140,0) == 12
+    return
+
+if __name__ == '__main__':
+    main()
