184c06ddf881787e204a32034fbc901fb9992c7f
nkyono <38087905+nkyono@users.noreply.github.com>
2021-04-28 00:05:35 -0700

    Update linkedList.go

--- test/linkedList/linkedList.go
+++ test/linkedList/linkedList.go
@@ -2,18 +2,45 @@ package main
 
 import "fmt"
 
-type node struct {
+type nodeDouble struct {
 	val  int
-	next *node
+	next *nodeDouble
+	prev *nodeDouble
 }
 
-func main() {
-	values := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	first := node{-1, nil}
+func doubleLinkedList(values []int) {
+	first := nodeDouble{values[0], nil, nil}
+	curr := &first
+	prev := curr
+	for _, v := range values {
+		newNode := nodeDouble{v, nil, curr}
+		curr.next = &newNode
+		prev = curr
+		curr = curr.next
+		curr.prev = prev
+	}
+	curr.next = &first
+	prev = curr
+	curr = curr.next
+	curr.prev = prev
+	for i := 0; i < len(values); i++ {
+		fmt.Printf("%v\n", curr)
+		curr = curr.next
+	}
+
+}
+
+type nodeSingle struct {
+	val  int
+	next *nodeSingle
+}
+
+func singleLinkedList(values []int) {
+	first := nodeSingle{-1, nil}
 	head := &first
 	curr := head
 	for _, v := range values {
-		newNode := node{v, nil}
+		newNode := nodeSingle{v, nil}
 		curr.next = &newNode
 		curr = curr.next
 	}
@@ -22,4 +49,10 @@ func main() {
 		fmt.Printf("%v\n", head)
 		head = head.next
 	}
+}
+
+func main() {
+	values := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	singleLinkedList(values)
+	doubleLinkedList(values)
 }
